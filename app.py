c0e74f0e66f501aade73db03cb18488612154c39
Topi Pietilänaho <topi.pietilanaho99@gmail.com>
2025-11-05 20:44:49 +0200

    pyyttonikoodihubiin2

--- app.py
+++ app.py
@@ -5,19 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-# Connect to MySQL/MariaDB
-conn = mysql.connector.connect(
-host="localhost",
-user="exampleuser",
-password="change_this_strong_password",
-database="exampledb"
-)
-cursor = conn.cursor()
-cursor.execute("SELECT 'Hello from MySQL!'")
-result = cursor.fetchone()
-# Clean up
-cursor.close()
-conn.close()
-return f"<h1>{result[0]}</h1>"
+    # Connect to MySQL/MariaDB
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="exampleuser",
+        password="change_this_strong_password",
+        database="exampledb"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT 'Hello from MySQL!'")
+    result = cursor.fetchone()
+    # Clean up
+    cursor.close()
+    conn.close()
+    return f"<h1>{result[0]}</h1>"
+
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
